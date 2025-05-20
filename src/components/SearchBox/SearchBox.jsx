a59cf9d372c90276e55b44d0e0727a1f3a8a370d
777Pavel777 <cross1295@icloud.com>
2025-05-21 00:26:45 +0300

    add style

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -4,16 +4,17 @@ import styles from './SearchBox.module.css';
 
 function SearchBox() {
   const dispatch = useDispatch();
-  const filter = useSelector(state => state.filters.name);
+  const filter = useSelector((state) => state.filters.name);
 
   return (
     <div className={styles.container}>
+      <h2>Contacts</h2>
       <label htmlFor="filter">Find contacts by name</label>
       <input
         type="text"
         id="filter"
         value={filter}
-        onChange={e => dispatch(changeFilter(e.target.value))}
+        onChange={(e) => dispatch(changeFilter(e.target.value))}
         className={styles.input}
       />
     </div>
